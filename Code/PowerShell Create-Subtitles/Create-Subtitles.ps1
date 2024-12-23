9f8a7e0857204a0787d359245c25398b6608bc62
MHaensel73 <67669082+MHaensel73@users.noreply.github.com>
2024-12-23 03:47:16 -0800

    Minor updates to Create-Subtitles.ps1
    Cleaned up program flow slightly, removed "create .txt list of files" which was not needed in practice.

--- Code/PowerShell Create-Subtitles/Create-Subtitles.ps1
+++ Code/PowerShell Create-Subtitles/Create-Subtitles.ps1
@@ -1,47 +1,34 @@
 <#
 .SYNOPSIS
 Create-Subtitles generates .srt subtitle files for all media files in the current folder and all descendent folders.
+
 .DESCRIPTION
 Create-Subtitles generates .srt subtitle files for all media files in the current folder and all descendent folders.
 
 To do this:
 A list of all files contained in the current folder is generated recursively.
-The list is stored in D:\filelist.txt.
 The script iterates through the list of media files.
 For each media file:
 1. Change to the containing directory.
-2. Check if a .srt subtitle file has already been created for this media file. If not, generate one.
+2. Check if a .srt subtitle file has already been created. If not, create subtitles for the media file.
 3. If there’s an error condition, report that.
-After all files have been processed, change back to the D:\ directory.
+After all files have been processed, change back to the original working directory.
 
 Step 1 is done because the subtitle generation program has a hard time with very long path names. Changing to the parent folder lets us call the subtitle generator with only the file name, leaving off any additional path information.
 
-A media file list is intentionally left in D:\filelist.txt, since it is sometimes necessary to run the script multiple times to complete the operation. It should be deleted manually after successful completion.
 .EXAMPLE
 Create-Subtitles
+
 #>
 
 # Create Subtitles.ps1
 # Creates subtitles for all media files in a given folder
 
-# Where to store list of files
-$fileListLocation = 'D:\filelist.txt'
-
 # The script will change the current working directory (CWD) as part of normal operation
 # Directory to return to after the script finishes:
-$originalDir = 'D:\'
+$originalDir = $PWD.Path
 
-# Create a file list, or load one if it already exists
-if (! (Test-Path $fileListLocation)) {
-  cd 'D:\Data\Proud Boys Secure Chats'
-  $fileList = Get-ChildItem -recurse *.mp4,*.mov,*.avi,*.mkv
-  cd $originalDir
-
-  Set-content -Path $fileListLocation  -Value $fileList
-} else {
-  $fileList=get-content $fileListLocation
-}
-
+$fileList = Get-ChildItem -recurse *.mp4,*.mov,*.avi,*.mkv,*.flv
 
 # For each file in the file list . . .
 $fileList| ForEach-Object {
@@ -66,7 +53,7 @@ $fileList| ForEach-Object {
 
     # If the subtitle file does not already exist, create it
     if (! (Test-Path $subtitleFile)) {
-        # echo "Creating subtitle file $currentDir\$subtitleFile"
+        echo "Creating subtitle file $currentDir\$subtitleFile"
         "D:\Data\PortableApps\Whisper-Faster\whisper-faster.exe --language en --output_dir .\ --model large-v2 '$currentFileName' | Out-Null" | Invoke-Expression 
     }
 
